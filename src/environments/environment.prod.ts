ce875eb179a31b205f6bce77b1f25d216c60ae57
edutalkapp <binh@edutalkapp.com>
2021-03-07 12:26:18 +0700

    init

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,25 @@
+import { Environment } from '@abp/ng.core';
+
+const baseUrl = 'http://localhost:4200';
+
+export const environment = {
+  production: true,
+  application: {
+    baseUrl,
+    name: 'PMVOnline',
+    logoUrl: '',
+  },
+  oAuthConfig: {
+    issuer: 'https://localhost:44306',
+    redirectUri: baseUrl,
+    clientId: 'PMVOnline_App',
+    responseType: 'code',
+    scope: 'offline_access PMVOnline',
+  },
+  apis: {
+    default: {
+      url: 'https://localhost:44306',
+      rootNamespace: 'PMVOnline',
+    },
+  },
+} as Environment;
