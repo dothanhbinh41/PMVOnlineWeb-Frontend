1c218b5f0c216f70b66730b252a29c95e9a3e6e6
Nghia Ha Minh <nghia@naxam.net>
2021-04-08 22:34:53 +0700

    home with data

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -3,6 +3,7 @@ import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { TaskService } from '@proxy/tasks';
 import { OAuthService } from 'angular-oauth2-oidc';
+import * as moment from 'moment';
 import { finalize } from 'rxjs/operators';
 
 @Component({
@@ -37,8 +38,48 @@ export class HomeComponent implements OnInit {
       )
       .subscribe(data => {
         this.dataSource = data;
-        console.log(data)
+        console.log(data);
       });
+  }
+
+  toAction(actionType: number) {
+    switch (actionType) {
+      case 2:
+        return 'đã duyệt sự vụ';
+      case 7:
+        return 'thay đổi người phụ trách';
+      case 4:
+        return 'bình luận';
+      case 5:
+        return 'hoàn thành sự vụ';
+      case 0:
+        return 'yêu cầu giải quyết sự vụ';
+      case 9:
+        return 'theo dõi sự vụ';
+      case 6:
+        return 'không hoàn thành sự vụ';
+      case 3:
+        return 'không duyệt sự vụ';
+      case 8:
+        return 'mở lại sự vụ';
+      case 10:
+        return 'bỏ theo dõi sự vụ';
+      case 1:
+        return 'yêu cầu duyệt sự vụ';
+      default:
+        return '';
+    }
+  }
+
+  showDetail(item: any) {
+    console.log(item);
+  }
+
+  convertToLocalTime(time: string) {
+    const date = moment.utc(time).format('YYYY-MM-DD HH:mm:ss');
+    const stillUtc = moment.utc(date).toDate();
+    const local = moment(stillUtc).local().format('YYYY-MM-DD HH:mm:ss');
+    return local;
   }
 
   get hasLoggedIn(): boolean {
