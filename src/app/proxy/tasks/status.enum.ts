5b5a7c9e0c7cedfdc93419aaeb27bf33bb374a0d
edutalkapp <binh@edutalkapp.com>
2021-04-19 22:15:37 +0700

    update proxy

--- src/app/proxy/tasks/status.enum.ts
+++ src/app/proxy/tasks/status.enum.ts
@@ -7,6 +7,9 @@ export enum Status {
   Rejected = 3,
   Completed = 4,
   Incompleted = 5,
+  Rated = 6,
+  LeaderRated = 7,
+  Done = 8,
 }
 
 export const statusOptions = mapEnumToOptions(Status);
