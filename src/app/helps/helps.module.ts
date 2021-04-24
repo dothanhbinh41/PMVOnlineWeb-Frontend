0ef3fae1d08e36c14738e78d9afabeed697fbe97
nghiahaminh25101995 <nghiaanhit@gmail.com>
2021-04-24 18:14:55 +0700

    complete guide page

--- src/app/helps/helps.module.ts
+++ src/app/helps/helps.module.ts
@@ -1,10 +1,39 @@
 import { NgModule } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatNativeDateModule } from '@angular/material/core';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatListModule } from '@angular/material/list';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatSelectModule } from '@angular/material/select';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
 import { SharedModule } from '../shared/shared.module';
 import { HelpsRoutingModule } from './helps-routing.module';
 import { HelpsComponent } from './helps.component';
 
 @NgModule({
   declarations: [HelpsComponent],
-  imports: [SharedModule, HelpsRoutingModule],
+  imports: [
+    SharedModule, 
+    HelpsRoutingModule,
+    MatIconModule,
+    MatButtonModule,
+    MatProgressSpinnerModule,
+    MatDialogModule,
+    MatFormFieldModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    MatFormFieldModule,
+    MatSelectModule,
+    MatInputModule,
+    MatGridListModule,
+    MatSnackBarModule,
+    MatMenuModule,
+    MatListModule],
 })
 export class HelpsModule {}
