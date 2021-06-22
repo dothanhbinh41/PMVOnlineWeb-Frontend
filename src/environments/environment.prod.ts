ee563f663fb8dc0825572b76307ee84a177da69e
Binh Do <binh@edutalkapp.com>
2021-06-22 11:33:44 +0700

    update env

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 import { Environment } from '@abp/ng.core';
 
-const baseUrl = 'http://task.pmvina.com';
+const baseUrl = 'https://task.pmvina.com';
 
 export const environment = {
   production: true,
